9aadbec82c6448dcb8d187b2bed2b99803138cb8
Hai Tran <52912440+haitrvn@users.noreply.github.com>
2024-10-04 09:56:18 +0700

    ExperimentalForeignApi

--- shared/src/iosMain/kotlin/com/haitrvn/kam/KInterstitial.ios.kt
+++ shared/src/iosMain/kotlin/com/haitrvn/kam/KInterstitial.ios.kt
@@ -1,7 +1,9 @@
 package com.haitrvn.kam
 
 import com.haitrvn.kam.core.RootView
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@ExperimentalForeignApi
 actual class KInterstitial {
     actual fun showAd(activity: RootView) {
     }
--- shared/src/iosMain/kotlin/com/haitrvn/kam/KInterstitialLoader.ios.kt
+++ shared/src/iosMain/kotlin/com/haitrvn/kam/KInterstitialLoader.ios.kt
@@ -3,7 +3,9 @@ package com.haitrvn.kam
 import com.haitrvn.kam.KInterstitial
 import com.haitrvn.kam.KInterstitialListener
 import com.haitrvn.kam.core.KAdRequest
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@ExperimentalForeignApi
 actual class KInterstitialLoader {
     actual suspend fun load(
         adUnitId: String,
