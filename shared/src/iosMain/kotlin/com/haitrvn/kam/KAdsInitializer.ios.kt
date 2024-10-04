29cb940c63d0f8a340946ed34040341b84af0bb8
Hai Tran <52912440+haitrvn@users.noreply.github.com>
2024-10-04 10:15:41 +0700

    Test cocapod

--- shared/src/iosMain/kotlin/com/haitrvn/kam/KAdsInitializer.ios.kt
+++ shared/src/iosMain/kotlin/com/haitrvn/kam/KAdsInitializer.ios.kt
@@ -1,6 +1,12 @@
 package com.haitrvn.kam
 
+import cocoapods.Google_Mobile_Ads_SDK.GADMobileAds
+import kotlinx.cinterop.ExperimentalForeignApi
+
+@OptIn(ExperimentalForeignApi::class)
 actual class KAdsInitializer {
     actual fun initialize(onComplete: () -> Unit) {
+        GADMobileAds.sharedInstance().startWithCompletionHandler {
+        }
     }
 }
