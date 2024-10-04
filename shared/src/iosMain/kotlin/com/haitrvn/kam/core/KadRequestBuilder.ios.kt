6b6ac192bed981b191eb27ad8fa11a0783978915
Hai Tran <52912440+haitrvn@users.noreply.github.com>
2024-10-04 10:05:04 +0700

    import kotlinx.cinterop.ExperimentalForeignApi
    @ExperimentalForeignApi

--- shared/src/iosMain/kotlin/com/haitrvn/kam/core/KadRequestBuilder.ios.kt
+++ shared/src/iosMain/kotlin/com/haitrvn/kam/core/KadRequestBuilder.ios.kt
@@ -1,5 +1,7 @@
 package com.haitrvn.kam.core
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@ExperimentalForeignApi
 actual object KadRequestBuilder {
     actual fun build(): KAdRequest {
         TODO("Not yet implemented")
